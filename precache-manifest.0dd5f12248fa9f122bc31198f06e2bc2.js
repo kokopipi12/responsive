bde64be418f1f6c379feedac23ef0814d27de35c
kokopipi12 <kkopipi12@gmail.com>
2020-09-05 20:45:58 +0700

    Updates

---
+++ precache-manifest.0dd5f12248fa9f122bc31198f06e2bc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "701502aa77c87c6aef5336a4ea986c25",
+    "url": "/responsive/index.html"
+  },
+  {
+    "revision": "45e1937ad5a5d31fde65",
+    "url": "/responsive/static/css/main.13edbd6e.chunk.css"
+  },
+  {
+    "revision": "278d8ca72bb75c81bbfc",
+    "url": "/responsive/static/js/2.e5431c9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/responsive/static/js/2.e5431c9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e1937ad5a5d31fde65",
+    "url": "/responsive/static/js/main.048e236d.chunk.js"
+  },
+  {
+    "revision": "e1d48291853534efd179",
+    "url": "/responsive/static/js/runtime-main.c9fb6085.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/responsive/static/media/logo.5d5d9eef.svg"
+  }
+]);
